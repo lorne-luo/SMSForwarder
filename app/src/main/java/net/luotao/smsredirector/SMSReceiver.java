55fc5210bfc4e54fab0dde244f284583ac1390d5
Tao <git@luotao.net>
2016-05-22 18:13:06 +1000

    add send_sms() in onReceive()

--- app/src/main/java/net/luotao/smsredirector/SMSReceiver.java
+++ app/src/main/java/net/luotao/smsredirector/SMSReceiver.java
@@ -8,6 +8,23 @@ import android.util.Log;
 import android.widget.Toast;
 import android.telephony.gsm.SmsManager;
 import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.telstra.api.http.client.APICallBack;
+import com.telstra.api.http.client.HttpClient;
+import com.telstra.api.http.client.HttpContext;
+import java.util.Calendar;
+
+import com.telstra.api.models.AuthenticationResponse;
+import com.telstra.api.models.GetAuthenticationInput;
+import com.telstra.api.models.MessageStatus;
+import com.telstra.api.models.ResponseStatusEnum;
+import com.telstra.api.models.SendMessageRequest;
+import com.telstra.api.models.SendMessageResponse;
+import com.telstra.api.Configuration;
+import com.telstra.api.controllers.APIController;
 
 public class SMSReceiver extends BroadcastReceiver {
     public static final String TAG = "ImiChatSMSReceiver";
@@ -16,21 +33,6 @@ public class SMSReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-//        if (intent.getAction().equals(SMS_RECEIVED_ACTION)) {
-//            SmsMessage[] messages = getMessagesFromIntent(intent);
-//            for (SmsMessage message : messages) {
-//
-//                CharSequence text = "Hello toast!";
-//                int duration = Toast.LENGTH_LONG;
-//                Toast toast = Toast.makeText(context, text, duration);
-//                toast.show();
-//
-//                Log.i(TAG, message.getOriginatingAddress() + " : " +
-//                        message.getDisplayOriginatingAddress() + " : " +
-//                        message.getDisplayMessageBody() + " : " +
-//                        message.getTimestampMillis());
-//            }
-//        }
         if (SMS_RECEIVED_ACTION.equals(intent.getAction())) {
             StringBuilder sb = new StringBuilder();
             Bundle bundle = intent.getExtras();
@@ -42,16 +44,59 @@ public class SMSReceiver extends BroadcastReceiver {
                 }
 
                 for (SmsMessage curMsg : msg) {
-                    sb.append("You got the message From:【");
+                    sb.append("[");
                     sb.append(curMsg.getDisplayOriginatingAddress());
-                    sb.append("】Content：");
+                    sb.append("]：");
                     sb.append(curMsg.getDisplayMessageBody());
+                    this.send_sms(context,sb.toString());
                 }
-                Toast.makeText(context,
-                        "Got The Message:" + sb.toString(),
-                        Toast.LENGTH_LONG).show();
+//                Toast.makeText(context,
+//                        String.valueOf(msg.length)+" Got The Message:" + sb.toString(),
+//                        Toast.LENGTH_LONG).show();
             }
         }
+
+    }
+
+    private void send_sms(Context context,String body){
+        Configuration.initialize(context);
+        APIController controller = new APIController();
+        String token = controller.getAccessToken();
+
+        Date now = new Date();
+        if (now.after(Configuration.tokenExpiry)) {
+            try {
+                Thread.currentThread().sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        token += "\n" + dateFormat.format(Configuration.tokenExpiry);
+        Log.i("onReceive", "token = " + token);
+        Log.i("onReceive", "token expiry = " + dateFormat.format(Configuration.tokenExpiry));
+
+        try {
+            controller.sendSelfSMSAsync(body,
+                    new APICallBack<SendMessageResponse>() {
+                        public void onSuccess(HttpContext context, SendMessageResponse response) {
+                            Log.i("sendSelfSMSAsync", response.getMessageId());
+                        }
+
+                        public void onFailure(HttpContext context, Throwable error) {
+                            Log.i("sendSelfSMSAsync", "12312saaasdfasfsd");
+                        }
+                    });
+        } catch (JsonProcessingException error) {
+            Toast.makeText(context,
+                    "JsonProcessingException = " + error.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+
+        Toast.makeText(context,
+                "Token = " + token,
+                Toast.LENGTH_LONG).show();
     }
 
     public final SmsMessage[] getMessagesFromIntent(Intent intent) {
